21e3901c53f3defb0a34818953f5245036c5d611
lhomitenzing202 <tenzinglhomi15@gmail.com>
2024-02-20 14:14:45 +0545

    navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import ShopCategory from './page/ShopCategory'
+import Shop from './page/Shop'
+import { LoginSignup } from './page/LoginSignup'
+import { Product } from './page/Product'
+import Cart from './page/Cart'
 
 function App() {
 
   return (
     <>
-    <div className="App"></div>
+    <div>
+    <BrowserRouter>
+    <Navbar/>
+
+    <Routes>
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/mens' element={<ShopCategory category="men"/>}/>
+      <Route path='/womens' element={<ShopCategory category="women"/>}/>
+      <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+      <Route path='/product' element={<Product/>}>
+        <Route path=':ProdcuctId' element ={<Product/>}/>
+      </Route>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
+    </Routes>
+    </BrowserRouter>
+   
+    </div>
     </>
   )
 }
