c88493c258ba716ade85125acfb08280653b1dde
lhomitenzing202 <tenzinglhomi15@gmail.com>
2024-02-24 22:43:25 +0545

    bug fixing d1

--- src/components/Items/Item.jsx
+++ src/components/Items/Item.jsx
@@ -1,4 +1,4 @@
-import "../Assets/data"
+import product_data from '../Assets/data'
 import './Item.css'
 
 const Item = (props) => {
@@ -21,3 +21,36 @@ const Item = (props) => {
 }
 
 export default Item
+
+
+
+
+// import PropTypes from 'prop-types'; // Import PropTypes
+// import './Item.css';
+
+// const Item = (props) => {
+//   return (
+//     <div className="item">
+//       <img src={props.image} alt="" />
+//       <p>{props.name}</p>
+
+//       <div className="item-prices">
+//         <div className="item-price-new">
+//           {props.new_price}
+//         </div>
+//         <div className="item-price-old">
+//           {props.old_price}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// Item.propTypes = {
+//   image: PropTypes.string, 
+//   name: PropTypes.string, 
+//   new_price: PropTypes.number, 
+//   old_price: PropTypes.number, 
+// };
+
+// export default Item;
--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,24 +1,50 @@
-import './Popular.css'
-import data_product from '../Assets/data'
+// import './Popular.css'
+// import data_product from '../Assets/data'
 
 
+
+// const Popular = () => {
+//   return (
+//     <div className="popular">
+//         <h1>Popular in womens</h1>
+//         <hr/>
+//         <div className="popular-item">
+//             {data_product.map((Item,i)=>{
+//                 return<Item key={i} id ={Item.id} name={Item.name} image={Item.image} new_price={Item.new_price} old_price={Item.old_price}/>
+//             })}
+//         </div>
+
+//   </div>    
+//   )
+// }
+
+// export default Popular
+
+import './Popular.css';
+import data_product from '../Assets/data';
+import Item from '../Items/Item';
 
 const Popular = () => {
   return (
     <div className="popular">
-        <h1>Popular in womens</h1>
-        <hr/>
-        <div className="popular-item">
-            {data_product.map((Item,i)=>{
-                return<Item key={i} id ={Item.id} name={Item.name} image={Item.image} new_price={Item.new_price} old_price={Item.old_price}/>
-            })}
-        </div>
-
-  </div>    
-  )
+      <h1>POPULAR IN WOMEN</h1>
+      <hr/>
+      <div className="popular-item">
+        {data_product.map((item, index) => (
+          <Item
+            key={index}
+            itemName={item.name}
+            newPrice={item.new_price}
+            oldPrice={item.old_price}
+            imageUrl={item.image}
+          />
+        ))}
+      </div>
+    </div>    
+  );
 }
 
-export default Popular
+export default Popular;
 
         
    
