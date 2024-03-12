3d60d689d1bee3e2c42684661451cabb0a44ff8e
lhomitenzing202 <tenzinglhomi15@gmail.com>
2024-03-12 22:59:04 +0545

    bug solved

--- src/components/CartItems/CartItems.jsx
+++ src/components/CartItems/CartItems.jsx
@@ -14,13 +14,29 @@ const CartItems = () => {
     <div className="cartitems-format-main">
         <p>Products</p>
         <p>Title</p>
-        <p>Quantity</p>
         <p>Price</p>
+        <p>Quantity</p>
         <p>Remove</p>
     </div>
     <hr />
     {all_product.map((e) => {
     if (cartItems[e.id] > 0) 
+        return (
+    <div>
+            <div className="cartitems-format cartitems-format-main" key={e.id}>
+                <img src={e.image} alt="" className='carticon-product-icon'/>
+                <p>{e.name}</p>
+                <p>Nrs. {e.new_price}</p>
+                <button className='cartitems-quantity'>{cartItems[e.id]}</button>
+                <p>Nrs. {e.new_price * cartItems[e.id]}</p>
+                <img className='cart-items-remove-icon' src={remove_icon} onClick={() => { removeFromCart(e.id) }} alt="" />
+                 </div><hr/> </div>
+        );       
+    
+})}  
+
+{/* {all_product.map((e) => {
+    if (cartItems[e.id] > 0) {
         return (
             <div className="cartitems-format" key={e.id}>
                 <img src={e.image} alt="" className='carticon-product-icon'/>
@@ -28,11 +44,13 @@ const CartItems = () => {
                 <p>Nrs{e.new_price}</p>
                 <button className='cartitems-quantity'>{cartItems[e.id]}</button>
                 <p>{e.new_price * cartItems[e.id]}</p>
-                <img src={remove_icon} onClick={() => { removeFromCart(e.id) }} alt="" />
+                <img src={remove_icon} onClick={() => removeFromCart(e.id)} alt="" />
             </div>
-        );        
-    
-})}  
+        );
+    }
+    return null; 
+})} */}
+
    </div>
   )
 }
