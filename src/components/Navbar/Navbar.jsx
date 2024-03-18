fd3322cbc82747b2ff9917db6bc069e1be5530d1
Ten.je.202 <147829240+lhomitenzing202@users.noreply.github.com>
2024-03-18 13:57:00 +0545

    Update Navbar.jsx
    cart count test 1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,34 @@
 
-import { useState } from "react"
+import { useRef, useState } from "react"
 import "./Navbar.css"
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ShopContext } from '../../context/ShopContext';
+import navdropdown_icon from '../Assets/navdropdown.png'
+
+
 
 const Navbar = ()=> {
-  const[menu,setMenu]= useState("shop");
+  const [menu,setMenu]= useState("shop");
+  const {getTotalCartItems} = useContext(ShopContext);
+
+  const refMenu = useRef();
+
+  const dropDown_toggle =(e)=>{
+    refMenu.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
+
   return (
-    <>
       <div className="navbar">
 
         <div className="nav-logo">
           <img src="src/components/Assets/fashionhub4.PNG" alt="" />
           <p>Fashion-Hub</p>
         </div>
-        <ul className="nav-menu">
+
+        <img className="nav-dropdown" onClick={dropDown_toggle} src={navdropdown_icon} alt="" />
+        <ul rel={refMenu} className="nav-menu">
           <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
           <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration:'none'}} to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
           <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration:'none'}} to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
@@ -23,14 +38,10 @@ const Navbar = ()=> {
         <div className="nav-cart-login">
          <Link to='/login'><button>Login</button></Link> 
           <Link to='/cart'><img src="src\components\Assets\cart_icon.png" alt="" /></Link>
-
-          <div className="nav-cart-count">0</div>
+          <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
       </div>
-    </>
-
   )
 }
 
 export default Navbar
-
