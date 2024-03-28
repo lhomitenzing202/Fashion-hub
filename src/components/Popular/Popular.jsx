ce3e6f0cd7a9855735668a50f24cb8b37f29f8bd
lhomitenzing202 <tenzinglhomi15@gmail.com>
2024-03-28 23:25:12 +0545

    popular updated

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,24 +1,3 @@
-// import './Popular.css'
-// import data_product from '../Assets/data'
-
-
-
-// const Popular = () => {
-//   return (
-//     <div className="popular">
-//         <h1>Popular in womens</h1>
-//         <hr/>
-//         <div className="popular-item">
-//             {data_product.map((Item,i)=>{
-//                 return<Item key={i} id ={Item.id} name={Item.name} image={Item.image} new_price={Item.new_price} old_price={Item.old_price}/>
-//             })}
-//         </div>
-
-//   </div>    
-//   )
-// }
-
-// export default Popular
 
 import './Popular.css';
 import Item from '../Items/Item';
